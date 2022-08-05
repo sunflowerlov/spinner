103903ea65fc07a456f706d9b9268d1fa229cc7a
Lisha Liang <lishaliang1999@gmail.com>
2022-08-05 19:24:39 +0000

    one cricle

--- spinner1.js
+++ spinner1.js
@@ -1,40 +1,34 @@
-process.stdout.write('hello from spinner1.js... \rheyyy\n');
-
 setTimeout(() => {
   process.stdout.write('\r|   ');
 }, 100);
 
 setTimeout(() => {
   process.stdout.write('\r/   ');
-}, 300);
+}, 400);
 
 setTimeout(() => {
   process.stdout.write('\r-   ');
-}, 500);
-
-setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\   '); 
 }, 700);
 
 setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 1000);
+
+// ... fill in the rest yourself ...
+setTimeout(() => {
   process.stdout.write('\r|   ');
-}, 900);
-
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 1100);
-
-setTimeout(() => {
-  process.stdout.write('\r-   ');
 }, 1300);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\   '); 
-}, 1500);
+  process.stdout.write('\r/   ');
+}, 1600);
 
 setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 1700);
+  process.stdout.write('\r-   ');
+}, 1900);
 
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 2200);
